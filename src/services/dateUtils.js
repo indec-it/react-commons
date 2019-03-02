65c3864de7a2b359730afec34d94a91e0448fff4
Ariel Erviti <ariel.erviti.ae@gmail.com>
2019-03-02 20:20:22 -0300

    feat(datetime): add date time field

--- src/services/dateUtils.js
+++ src/services/dateUtils.js
@@ -1,8 +1,10 @@
 import {isDate} from 'lodash';
+import moment from 'moment';
 import 'moment/locale/es';
 
 export default class DateUtilsService {
     static LOCALE = 'es-AR';
+    static DATE_TIME_FORMAT = 'DD/MM/YYYY HH:mm:ss';
 
     static formatDate(input) {
         if (!input) {
@@ -22,5 +24,12 @@ export default class DateUtilsService {
 
     static formatDateTime(input) {
         return `${DateUtilsService.formatDate(input)} ${DateUtilsService.formatTime(input)}`;
+    }
+
+    static formatDateTimePicker(input) {
+        if (!input) {
+            return '';
+        }
+        return moment(input, DateUtilsService.DATE_TIME_FORMAT).format(DateUtilsService.DATE_TIME_FORMAT);
     }
 }
