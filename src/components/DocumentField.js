373cffe20bbcceafa27c84c2d56c9da51d9f5959
Maximiliano Forlenza <forlenzamaximiliano@gmail.com>
2019-06-10 10:00:03 -0300

    refactor: change class to function component

--- src/components/DocumentField.js
+++ src/components/DocumentField.js
@@ -1,55 +1,50 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import ValidatorService from '../services/validator';
 
 import TextField from './TextField';
 
-class DocumentField extends Component {
-    static propTypes = {
-        control: PropTypes.string.isRequired,
-        label: PropTypes.string.isRequired,
-        value: PropTypes.string.isRequired,
-        onChange: PropTypes.func.isRequired,
-        disabled: PropTypes.bool
-    };
-
-    static defaultProps = {
-        disabled: false
-    };
-
-    validateInput() {
-        if (!this.props.value) {
-            return null;
-        }
-        return ValidatorService.validateDocument(this.props.value) ? 'success' : 'error';
+const validateInput = value => {
+    if (!value) {
+        return null;
     }
+    return ValidatorService.validateDocument(value) ? 'success' : 'error';
+};
 
-    handleChange({target}) {
-        if (this.props.value === target.value) {
-            return;
-        }
-        this.props.onChange({target});
+const handleChange = ({target}, value, onChange) => {
+    if (value === target.value) {
+        return;
     }
+    onChange({target});
+};
 
-    render() {
-        const {
-            control, label, value, disabled
-        } = this.props;
-        return (
-            <TextField
-                control={control}
-                label={label}
-                value={value}
-                required
-                minLength={7}
-                maxLength={8}
-                disabled={disabled}
-                onChange={e => this.handleChange(e)}
-                validateInput={this.validateInput()}
-            />
-        );
-    }
-}
+const DocumentField = ({
+    control, label, value, disabled, onChange
+}) => (
+    <TextField
+        control={control}
+        label={label}
+        value={value}
+        required
+        minLength={7}
+        maxLength={8}
+        disabled={disabled}
+        onChange={e => handleChange(e, value, onChange)}
+        validateInput={validateInput(value)}
+    />
+);
+
+DocumentField.propTypes = {
+    control: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+    disabled: PropTypes.bool
+};
+
+DocumentField.defaultProps = {
+    disabled: false
+};
 
 export default DocumentField;
