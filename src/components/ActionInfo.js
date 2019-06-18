f1ae650ae50e79eb91096853e476f9849bb50327
Pablo Bastidas Carbonell <p.bastidascarbonell@gmail.com>
2019-06-18 18:44:18 -0300

    style(indicators): they were stylized compnents indicators and info

--- src/components/ActionInfo.js
+++ src/components/ActionInfo.js
@@ -2,37 +2,37 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {Toast, ToastBody, ToastHeader} from 'reactstrap';
 import {faExclamationTriangle} from '@fortawesome/free-solid-svg-icons';
-import {childrenPropTypes, fontAwesomeIconPropTypes} from '../util';
 
+import {childrenPropTypes, fontAwesomeIconPropTypes} from '../util';
 import Icon from './Icon';
 
-const IconButton = ({
-    icon, pullIconRight, children
-}) => (
-    <div className="p-3 bg-info my-2 rounded">
-        <Toast>
-            <ToastHeader>
-                <Icon icon={faExclamationTriangle}>
-                    Información
-                </Icon>
-            </ToastHeader>
-            <ToastBody>
-                <Icon {...{pullIconRight, icon, children}}/>
-            </ToastBody>
-        </Toast>
-    </div>
+const ActionInfo = ({
+    icon, show, pullIconRight, children
+}) => show && (
+    <Toast className="mx-auto w-50 p-3 text-white text-center">
+        <ToastHeader>
+            <Icon icon={faExclamationTriangle}>
+                Información
+            </Icon>
+        </ToastHeader>
+        <ToastBody>
+            <Icon {...{pullIconRight, icon, children}}/>
+        </ToastBody>
+    </Toast>
 );
 
-IconButton.propTypes = {
+ActionInfo.propTypes = {
+    show: PropTypes.bool,
     icon: fontAwesomeIconPropTypes,
     pullIconRight: PropTypes.bool,
     children: childrenPropTypes
 };
 
-IconButton.defaultProps = {
+ActionInfo.defaultProps = {
+    show: false,
     icon: undefined,
     pullIconRight: false,
     children: undefined
 };
 
-export default IconButton;
+export default ActionInfo;
