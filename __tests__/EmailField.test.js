bbf8c0b6b83bc62e80182ebac18a1b3e3caa8d1c
Maximiliano Forlenza <forlenzamaximiliano@gmail.com>
2019-07-11 19:40:59 -0300

    test(emailField): add unit test

--- __tests__/EmailField.test.js
+++ __tests__/EmailField.test.js
@@ -5,18 +5,28 @@ import EmailField from '../src/components/EmailField';
 
 const onChange = jest.fn();
 
-const renderComponent = (props = {}) => {
-    const wrapper = shallow(
-        <EmailField label="emailField" value="test@test.com" control="email" onChange={onChange} {...props}/>
-    );
-    wrapper.input = () => wrapper.find('FormGroup Input');
-    return wrapper;
-};
+const renderComponent = (props = {}) => shallow(
+    <EmailField label="emailField" value="test@test.com" control="email" onChange={onChange} {...props}/>
+);
 
 describe('<EmailField/>', () => {
-    it('should return undefined if props value is equal to new value', () => {
-        const wrapper = renderComponent();
-        wrapper.input().simulate('change', {target: {value: 'test@test.com'}});
-        const handleChange = wrapper.instance().handleChange;
+    describe('handleChange', () => {
+        it('should return undefined if props value is equal to new value', () => {
+            const wrapper = renderComponent();
+            const handleChange = wrapper.instance().handleChange({target: {value: wrapper.props().value}});
+            expect(handleChange).toBeUndefined();
+        });
+        it('should return true if email is valid', () => {
+            const wrapper = renderComponent();
+            wrapper.instance().handleChange({target: {value: 'test@test.com.ar'}});
+            const isValid = wrapper.state().valid;
+            expect(isValid).toBe(true);
+        });
+        it('should return false if email is invalid', () => {
+            const wrapper = renderComponent();
+            wrapper.instance().handleChange({target: {value: 'test'}});
+            const isValid = wrapper.state().valid;
+            expect(isValid).toBe(false);
+        });
     });
 });
