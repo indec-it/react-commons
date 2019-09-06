d5ca105442414ce612b89b1462f9da0e7549153f
Maximiliano Forlenza <forlenzamaximiliano@gmail.com>
2019-09-06 13:43:44 -0300

    test(actionIndicator): change test

--- __tests__/ActionIndicator.test.js
+++ __tests__/ActionIndicator.test.js
@@ -5,29 +5,13 @@ import ActionIndicator from '../src/components/ActionIndicator';
 
 const renderComponent = (props = {}) => {
     const wrapper = shallow(<ActionIndicator {...props}/>);
-    wrapper.indicator = () => wrapper.find('Fragment div div').children();
+    wrapper.indicator = () => wrapper.find('Modal').children();
     return wrapper;
 };
 
 describe('<ActionIndicator/>', () => {
-    it('should render the component if has prop saving', () => {
-        const wrapper = renderComponent({saving: true});
-        expect(wrapper.isEmptyRender()).toBe(false);
-    });
-    it('should render the component if has prop loading', () => {
-        const wrapper = renderComponent({loading: true});
-        expect(wrapper.isEmptyRender()).toBe(false);
-    });
-    it('should be empty component if doesnt exist prop loading or saving', () => {
+    it('should match snapshot', () => {
         const wrapper = renderComponent();
-        expect(wrapper.isEmptyRender()).toBe(true);
-    });
-    it('should render LoadingIndicator component if loading flag exists', () => {
-        const wrapper = renderComponent({loading: true});
-        expect(wrapper.indicator()).toMatchSnapshot();
-    });
-    it('should render SaveIndicator component if saving flag exists', () => {
-        const wrapper = renderComponent({saving: true});
-        expect(wrapper.indicator()).toMatchSnapshot();
+        expect(wrapper).toMatchSnapshot();
     });
 });
