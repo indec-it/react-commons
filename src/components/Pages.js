227b1e2ff2ec57f33f761fd1dc1ffe9cd5cd520b
Maximiliano Forlenza <forlenzamaximiliano@gmail.com>
2019-09-19 21:01:49 -0300

    fix(pages): add paginationLink to show color

--- src/components/Pages.js
+++ src/components/Pages.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
-    Col, Row, Pagination, PaginationItem
+    Col, Row, Pagination, PaginationItem, PaginationLink
 } from 'reactstrap';
 import {ceil, min, range} from 'lodash';
 
 const getPages = (pageSize, resultsCount, maxPage) => range(min([ceil(resultsCount / pageSize), maxPage]));
 
 const Pages = ({
-    pageSize, resultsCount, selectedPage, maxPage, onChange
+    pageSize, resultsCount, selectedPage, maxPage, onChange, paginationSize
 }) => (
     <Row>
-        <Col sm={12} className="text-center">
-            <Pagination bsSize="medium">
+        <Col>
+            <Pagination size={paginationSize}>
                 {getPages(pageSize, resultsCount, maxPage).map(page => (
                     <PaginationItem key={page} active={page === selectedPage} onClick={() => onChange(page)}>
-                        {page + 1}
+                        <PaginationLink>{page + 1}</PaginationLink>
                     </PaginationItem>
                 ))}
             </Pagination>
@@ -28,13 +28,15 @@ Pages.propTypes = {
     resultsCount: PropTypes.number,
     pageSize: PropTypes.number,
     selectedPage: PropTypes.number.isRequired,
-    maxPage: PropTypes.number
+    maxPage: PropTypes.number,
+    paginationSize: PropTypes.string
 };
 
 Pages.defaultProps = {
     maxPage: 30,
     pageSize: 0,
-    resultsCount: 0
+    resultsCount: 0,
+    paginationSize: 'md'
 };
 
 export default Pages;
